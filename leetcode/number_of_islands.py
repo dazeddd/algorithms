5c61fdd7b0630d2d4ad92576153744e633641528
dazeddd <joonsoolee14@gmail.com>
2023-11-15 13:25:57 +0900

    add README

---
+++ leetcode/number_of_islands.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def dfs(self, grid: List[List[str]], i: int, j: int):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or \
+            grid[i][j] != '1':
+                return
+        
+        grid[i][j] = '0'
+
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
+            
