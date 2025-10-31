39d77d5bd8dab61a90672c038327687396319bfe
Joonsoo Lee <joonsoolee14@gmail.com>
2025-10-31 18:01:07 +0900

    testdome

---
+++ testdome/REAME.md
@@ -0,0 +1,6 @@
+
+- public, python questions
+
+https://www.testdome.com/library?page=1&skillArea=33&questionSets=public
+
+
--- testdome/boat_movements.py
+++ testdome/boat_movements.py
@@ -1,21 +1,18 @@
 # https://www.testdome.com/questions/python/boat-movements/135037
 
 def can_travel_to(game_matrix, from_row, from_column, to_row, to_column):
-
-    
-
     pass
 
 if __name__ == "__main__":
     game_matrix = [
-        [False, False, True, True, False],
-        [False, False, True, False, False],
-        [False, False, True, True, False],
-        [False, True, False, True, False],
-        [False, False, True, False, False]
+        [False, True,  True,  False, False, False],
+        [True,  True,  True,  False, False, False],
+        [True,  True,  True,  True,  True,  True],
+        [False, True,  True,  False, True,  True],
+        [False, True,  True,  True,  False, True],
+        [False, False, False, False, False, False],
     ]
 
-    print(can_travel_to(game_matrix, 2, 2, 0, 2))
-    print(can_travel_to(game_matrix, 2, 2, 2, 1))
-    print(can_travel_to(game_matrix, 2, 2, 2, 3))
-    print(can_travel_to(game_matrix, 2, 2, 4, 2))
+    print(can_travel_to(game_matrix, 3, 2, 2, 2)) # True, Valid move
+    print(can_travel_to(game_matrix, 3, 2, 3, 4)) # False, Can't travel through land
+    print(can_travel_to(game_matrix, 3, 2, 6, 2)) # False, Out of bounds
