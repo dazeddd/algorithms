c98b9859fcd79af578ff8659f8348dd2340138ab
Joonsoo Lee <joonsoolee14@gmail.com>
2025-11-07 07:41:15 +0900

    codility

--- codility/tennis_tournament.py
+++ codility/tennis_tournament.py
@@ -7,4 +7,17 @@ https://app.codility.com/programmers/trainings/3/tennis_tournament/
 
 def solution(P, C):
     # Implement your solution here
-    pass
+
+    if P >= C * 2:
+        return C
+    else:
+        return P // 2
+
+    # pass
+
+if __name__ == "__main__":
+    # ret = solution(10,3)
+    ret = solution(5,3)
+    print(ret)
+
+
