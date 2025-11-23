7e4eb552f7663b97697902b661a344e3fa11688c
Joonsoo Lee <joonsoolee14@gmail.com>
2025-11-23 16:48:57 +0900

    restruct

--- leetcode/best_time_to_buy_and_sell_stock_2.py
+++ leetcode/best_time_to_buy_and_sell_stock_2.py
@@ -2,7 +2,6 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
         profits = []
         for i in range(len(prices)-1):
             if prices[i] >= prices[i+1]:
--- leetcode/snippet.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-
-    def foo(self):
-        pass
-
-if __name__ == "__main__":
-    sol = Solution()
-    answer = sol.foo()
