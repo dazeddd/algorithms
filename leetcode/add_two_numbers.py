6604d69c590545b181b0f31763fa3359b5e0e757
Joonsoo Lee <joonsoolee14@gmail.com>
2025-11-26 14:36:36 +0900

    restruct

--- leetcode/add_two_numbers.py
+++ leetcode/add_two_numbers.py
@@ -1,41 +1,21 @@
 # Definition for singly-linked list.
 from typing import Optional
 
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
 class Solution:
-    def addTwoNumbers(self, 
-        l1: Optional[ListNode],
-        l2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        # l1_values = []
-        # l2_values = []
+        curr = ListNode(0)
+        carry = 0
 
-        # l1_value = 0
-        # l1_exponent = 0
-        
-        # l2_value = 0
-        # l2_exponent = 0
-
-        # while l1 is not None:
-        #     # l1_values.append(l1.val)
-        #     l1_value += l1.val * pow(10, l1_exponent)
-        #     l1_exponent += 1
-
-        #     l1 = l1.next
-        
-        # while l2 is not None:
-        #     # l2_values.append(l2.val)
-        #     l2_value += l2.val * pow(10, l2_exponent)
-        #     l2_exponent += 1
+        while l1 or l2 or carry != 0:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
             
-        #     l2 = l2.next
-        
-        # answer = l1_value + l2_value
-        # return answer
 
 if __name__ == "__main__":
     sol = Solution()
--- leetcode/best_time_to_buy_and_sell_stock_2.py
+++ leetcode/best_time_to_buy_and_sell_stock_2.py
@@ -3,6 +3,7 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profits = []
+        
         for i in range(len(prices)-1):
             if prices[i] >= prices[i+1]:
                 continue
