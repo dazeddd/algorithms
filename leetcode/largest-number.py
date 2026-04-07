d5dee550e72f10bef8d2a36af4f27f450376b6c4
Joonsoo <joonsoo@Joonsooui-MacBookAir.local>
2026-04-07 14:54:43 +0900

    leetcode
    Committer: Joonsoo Lee <joonsoo@Joonsooui-MacBookAir.local>

---
+++ leetcode/largest-number.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        return
+    
+if __name__ == "__main__":
+    sol = Solution()
+
+    nums = [3,30,34,5,9]
+    answer = sol.largestNumber(nums=nums)
+
+    # assert answer
---
+++ leetcode/reverse-integer.py
@@ -0,0 +1,29 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        tmp_str = str(x)
+
+        if tmp_str[0] == '-':
+            base = "-"
+            tmp_str = str(x)[1:]
+        else:
+            base = ""
+            tmp_str = str(x)
+        
+        for idx in range(len(tmp_str)):
+            base += tmp_str[-idx-1]
+
+        # print(base)
+        reversed = int(base)
+        if reversed < (2 ** 31) and reversed >= -(2 ** 31):
+            return int(base)
+        else: 
+            return 0
+    
+
+    
+if __name__ == "__main__":
+    sol = Solution()
+    # result = sol.reverse(x=123)
+    # result = sol.reverse(x=120)
+    result = sol.reverse(x=-123)
+    print(result)
