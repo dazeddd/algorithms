754581d0bb97fc5b001b65fff12f06d584fb9dfd
Joonsoo Lee <joonsoolee14@gmail.com>
2026-04-13 15:37:34 +0900

    leetcode

---
+++ leetcode/longest-consecutive-sequence.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        return
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    
+    # Output: 9
+    result = sol.longestConsecutive(nums=[0,3,7,2,5,8,4,6,0,1])
---
+++ leetcode/word-break.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        # for word in wordDict
+
+        
+        return
+    
+
+if __name__ == "__main__":
+    
+    sol = Solution()
+    
+    # Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    sol.wordBreak(s=s, wordDict=wordDict)
